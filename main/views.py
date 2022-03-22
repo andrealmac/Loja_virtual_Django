c1cebca5068ba80bddb58a64e20452c5327dc7d1
ANDRE AUGUSTO DE ALMEIDA MACHADO <49785448+andrealmac@users.noreply.github.com>
2022-03-22 20:47:55 -0300

    Aula3

--- main/views.py
+++ main/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render, HttpResponse #HttpResponse para usar o requ
 
 # Vai ser visto pelo usuario
 def homepage(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    #return HttpResponse('<h1>Hello World</h1>') 
+    return render(request, template_name='main/home.html')#montar um arquivo em html e referenciando o caminho atraves do template
+    
+def itemspage(request):
+    return render(request, template_name='main/items.html')
