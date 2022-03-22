26b38f9f7861a9fab68c52c2b1418fb6f5b6736a
ANDRE AUGUSTO DE ALMEIDA MACHADO <49785448+andrealmac@users.noreply.github.com>
2022-03-22 20:24:31 -0300

    Aula1

---
+++ main/urls.py
@@ -0,0 +1,7 @@
+#nome da pagina e onde chama elas
+from django.urls import path #caminho que a urls da pasta shop ira usar
+
+urlpatterns = [
+    #path('admin/', admin.site.urls),
+    #path('', include('main.urls')),#chamando a urls da pasta main(APP)
+]
