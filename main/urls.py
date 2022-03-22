1f16b43ff02713c3bd158d1aff7a38e76cd933e5
ANDRE AUGUSTO DE ALMEIDA MACHADO <49785448+andrealmac@users.noreply.github.com>
2022-03-22 20:56:51 -0300

    Aula4

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,5 @@ from main import views#Chamar o arquivo views da pasta views(main)
 urlpatterns = [
     path('home/', views.homepage, name='home'),#caminho e o nome do caminho na pagina
     #path('', include('main.urls')),#chamando a urls da pasta main(APP)
+    path('items/', views.itemspage, name='items'),
 ]
--- main/views.py
+++ main/views.py
@@ -6,4 +6,14 @@ def homepage(request):
     return render(request, template_name='main/home.html')#montar um arquivo em html e referenciando o caminho atraves do template
     
 def itemspage(request):
-    return render(request, template_name='main/items.html')
+    items = [
+        {
+            'name':'Telefone',
+            'price':'500'
+        },
+        {
+            'name':'Notebook',
+            'price':'2500'
+        }
+    ]
+    return render(request, template_name='main/items.html', context={'items':items})#passar os items para o template
