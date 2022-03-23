1373293d87ea77709ac341b24891cf78ec8fc7db
ANDRE AUGUSTO DE ALMEIDA MACHADO <49785448+andrealmac@users.noreply.github.com>
2022-03-23 15:19:11 -0300

    Aula8

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
-# Create your models here.
+#python manage.py migrate
+# Criar as classes funcoes e carregar tudo para o banco de dados
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.CharField(max_length=1000)
+    image_url = models.CharField(max_length=512)
+    
--- main/views.py
+++ main/views.py
@@ -1,19 +1,7 @@
-from django.shortcuts import render, HttpResponse #HttpResponse para usar o request
+from django.shortcuts import render 
 
 # Vai ser visto pelo usuario
 def homepage(request):
-    #return HttpResponse('<h1>Hello World</h1>') 
-    return render(request, template_name='main/home.html')#montar um arquivo em html e referenciando o caminho atraves do template
-    
+    return render(request, template_name='main/home.html')
 def itemspage(request):
-    items = [
-        {
-            'name':'Telefone',
-            'price':'500'
-        },
-        {
-            'name':'Notebook',
-            'price':'2500'
-        }
-    ]
-    return render(request, template_name='main/items.html', context={'items':items})#passar os items para o template
+    return render(request, template_name='main/items.html')
