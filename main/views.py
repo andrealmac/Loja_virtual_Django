c069f0c2a3b7fb3737d145ce56c0cf043c23c8c1
ANDRE AUGUSTO DE ALMEIDA MACHADO <49785448+andrealmac@users.noreply.github.com>
2022-03-23 15:50:43 -0300

    Aula10

--- main/views.py
+++ main/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render 
 
+#importar os dados dos items no banco de dados
+from main.models import Item
+
 # Vai ser visto pelo usuario
 def homepage(request):
     return render(request, template_name='main/home.html')
 def itemspage(request):
-    return render(request, template_name='main/items.html')
+    items = Item.objects.all()
+    return render(request, template_name='main/items.html', context={'items':items})
